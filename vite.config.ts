d88996574609d2aaecf0f9f727a59199304efca1
NETiSACK <218697736+NETiSACK@users.noreply.github.com>
2025-08-29 14:31:07 +0700

    vite.config.ts: add base url

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: 'promptpay-qrcode-maker',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
